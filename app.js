58e56a96ee72af128952e0be960ede0387576e79
ninata11 <talaxadze2001@gmail.com>
2024-05-06 21:07:13 +0400

    add progress bars

--- app.js
+++ app.js
@@ -23,3 +23,38 @@ let sliderIntervalId = null;
 
 sliderIntervalId = setInterval(goToNextSlide, 5000);
 
+
+
+// progress bar section
+const progressSection = document.querySelector(".progress-section");
+const progressBars=document.querySelectorAll(".progress-bar");
+const progress=document.querySelector(".progress");
+
+function showProgress(){
+    progressBars.forEach(progressBar=>{
+        const value = progressBar.dataset.progress;
+        console.log(value);
+        progressBar.style.opacity=1;
+        progressBar.style.width= `${value}%`;
+    })
+}
+function hideProgress(){
+    progressBars.forEach(p=>{
+        p.style.opacity=0;
+        p.style.width=0;
+    })
+}
+
+
+window.addEventListener('scroll',()=>{
+    const sectionPos = progressSection.getBoundingClientRect().top;
+    const screenPos = window.innerHeight ;
+
+    if(sectionPos<screenPos){
+      showProgress();
+    }else{
+        hideProgress();
+    }
+})
+
+
